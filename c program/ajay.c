6e22c8a7ca57b1f10febd6ed4192c4d19ad00026
Chodipilli Ajay <117914960+ajaych1234@users.noreply.github.com>
2023-02-26 13:02:21 +0530

    add initial pv website files

---
+++ c program/ajay.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter the values of a,b,c,d:");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+printf("the values of a,b,c,d are:%d,%d,%d,%d.",a,b,c,d);
+return 0;
+}
